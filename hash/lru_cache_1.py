82e1e28f31af50910652d9879294dcca86d4b278
Rachit <rachit2311@gmail.com>
2024-03-13 12:44:43 +0100

    Leetcode 146 - LRU Cache 1

---
+++ hash/lru_cache_1.py
@@ -0,0 +1,31 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.c = capacity
+        self.cache = deque()
+        self.map = {}
+
+    def get(self, key: int) -> int:
+        if key in self.map:
+            self.cache.remove(key)
+            self.cache.append(key)
+            value = self.map[key]
+            return value
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key not in self.map:
+            if self.c == len(self.cache):
+                oldest = self.cache.popleft()
+                del self.map[oldest]
+        else:
+            self.cache.remove(key)
+        self.cache.append(key)
+        self.map[key] = value
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
