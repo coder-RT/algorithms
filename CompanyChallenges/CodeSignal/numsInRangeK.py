973a5bc6e15957d38b7b0374624bf6270221e188
Rachit <rachit2311@gmail.com>
2024-03-14 23:09:15 +0100

    Code Signal: index within range k

---
+++ CompanyChallenges/CodeSignal/numsInRangeK.py
@@ -0,0 +1,15 @@
+"""
+Given an array of integers nums and an integer k, 
+determine whether there are two distinct indices i and j in the array where nums[i] = nums[j] and the absolute difference between i and j is less than or equal to k.
+"""
+def solution(nums, k):
+    m = {}
+    n = len(nums)
+
+    for i in range(n):
+        if nums[i] in m and i - m[nums[i]] <= k:
+            #if (i - m[nums[i]]) <= k:
+            return True
+        else:
+            m[nums[i]] = i
+    return False
